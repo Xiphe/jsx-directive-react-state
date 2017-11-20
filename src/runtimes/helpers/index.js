b007a9cb34ffd23504efb267e990630aba543276
Hannes Diercks <github@xiphe.net>
2017-11-20 23:01:03 +0100

    style: please linter

--- src/runtimes/helpers/index.js
+++ src/runtimes/helpers/index.js
@@ -1,1 +1,3 @@
+/* this is a collection file, having a default export makes no sense */
+/* eslint-disable import/prefer-default-export */
 export { default as createBootstrap } from './createBootstrap';
